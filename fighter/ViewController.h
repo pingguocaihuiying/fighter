bbb7035ef9a52cb167e2c104d4f7cbbb90ee0d5d
liyz <liyz@mapbar.com>
2016-04-07 17:34:59 +0800

    Initial Commit

---
+++ fighter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fighter
+//
+//  Created by Liyz on 4/7/16.
+//  Copyright © 2016 Mapbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fighterTests/fighterTests.m
@@ -0,0 +1,39 @@
+//
+//  fighterTests.m
+//  fighterTests
+//
+//  Created by Liyz on 4/7/16.
+//  Copyright © 2016 Mapbar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fighterTests : XCTestCase
+
+@end
+
+@implementation fighterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
