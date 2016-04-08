796e8903b6a7899a3608e7ad44dee6ca96ae14de
liyz <liyz@mapbar.com>
2016-04-08 11:06:12 +0800

    创建baseVC

---
+++ fighter/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  fighter
+//
+//  Created by Liyz on 4/8/16.
+//  Copyright © 2016 Mapbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UITabBarController
+
+@end
---
+++ fighter/InformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.h
+//  fighter
+//
+//  Created by Liyz on 4/8/16.
+//  Copyright © 2016 Mapbar. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface InformationViewController : BaseViewController
+
+@end
