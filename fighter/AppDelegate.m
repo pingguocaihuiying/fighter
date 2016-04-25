8ed1ee969477d2ed425a0dab557dae70ea193fc8
liyz <445793016@qq.com>
2016-04-25 12:55:58 +0800

    test

--- fighter/AppDelegate.m
+++ fighter/AppDelegate.m
@@ -94,7 +94,7 @@
         }];
     }
 }
-//test222
+//test22233
 
 - (void)getWechatUserInfoWithToken:(NSString *)token andOpenId:(NSString *)openId{
     NSString *stringURL = [NSString stringWithFormat:@"https://api.weixin.qq.com/sns/userinfo?access_token=%@&openid=%@", token, openId];
