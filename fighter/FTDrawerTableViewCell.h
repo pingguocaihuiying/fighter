57140addc7599b21c5a4a6698969d43535d223ce
guoyuyanmeng <kangxiaoqiangdesky@163.com>
2016-04-27 11:35:03 +0800

    侧滑栏---未完成

---
+++ fighter/FTDrawerTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  FTDrawerTableViewCell.h
+//  fighter
+//
+//  Created by kang on 16/4/26.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTDrawerTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@end
---
+++ fighter/FTDrawerViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTDrawerViewController.h
+//  fighter
+//
+//  Created by kang on 16/4/27.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTDrawerViewController : UIViewController
+
+@end
