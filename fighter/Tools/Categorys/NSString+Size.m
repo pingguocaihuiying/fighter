3f4f0050b710734422aa4494dee5149a1f6a29e8
guoyuyanmeng <kangxiaoqiangdesky@163.com>
2016-04-28 14:26:55 +0800

    系统九宫格中文键盘bug处理

---
+++ fighter/Tools/Categorys/NSString+Size.m
@@ -0,0 +1,19 @@
+//
+//  NSString+Size.m
+//  fighter
+//
+//  Created by kang on 16/4/27.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import "NSString+Size.h"
+
+@implementation NSString (Size)
+
+//返回字符串所占用的尺寸.
+-(CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize
+{
+    NSDictionary *attrs = @{NSFontAttributeName : font};
+    return [self boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+}
+@end
