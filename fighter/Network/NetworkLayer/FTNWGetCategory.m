c4145e3fde5fb42fe20b310e02c5829c77de7d82
liyz <445793016@qq.com>
2016-05-30 19:38:08 +0800

    临时解决了获取标签崩溃的问题

--- fighter/Network/NetworkLayer/FTNWGetCategory.m
+++ fighter/Network/NetworkLayer/FTNWGetCategory.m
@@ -32,8 +32,8 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         [FTNWGetCategory getCategoryWithOption:^(NSArray *array) {
-            [[NSUserDefaults standardUserDefaults]setObject:array forKey:CATEGORIES];
-            [[NSUserDefaults standardUserDefaults]synchronize];
+//            [[NSUserDefaults standardUserDefaults]setObject:array forKey:CATEGORIES];
+//            [[NSUserDefaults standardUserDefaults]synchronize];
         }];
     });
     return resultArray;
@@ -47,7 +47,7 @@
     //设置请求返回的数据类型为默认类型（NSData类型)
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     NSString *getCategoryUrlString = [FTNetConfig host:Domain path:GetCategoryURL];
-//    NSLog(@"getCategoryUrlString : %@", getCategoryUrlString);
+    NSLog(@"getCategoryUrlString : %@", getCategoryUrlString);
     
     [manager POST:getCategoryUrlString parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
         NSMutableArray *resultArray;
