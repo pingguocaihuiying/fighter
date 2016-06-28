f509ac5c104d224365ebf2490148240344398997
kangxq <1515843677@qq.com>
2016-06-28 14:04:00 +0800

    123

---
+++ fighter/CycleScrollView.m
@@ -0,0 +1,14 @@
+//
+//  CycleScrollView.m
+//  fighter
+//
+//  Created by kang on 16/6/28.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import "CycleScrollView.h"
+
+@implementation CycleScrollView
+
+
+@end
--- fighter/FTCoachView.m
+++ fighter/FTCoachView.m
@@ -120,7 +120,7 @@
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    return 120;
+    return 100;
 }
 //headerView高度
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)sectio{
