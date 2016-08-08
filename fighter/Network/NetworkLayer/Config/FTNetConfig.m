b460ad92f835b6708d8e812df6e9d5a7ed7c255c
kangxq <1515843677@qq.com>
2016-08-08 11:10:37 +0800

    123

--- fighter/Network/NetworkLayer/Config/FTNetConfig.m
+++ fighter/Network/NetworkLayer/Config/FTNetConfig.m
@@ -46,9 +46,10 @@
 //NSString * const Domain = @"http://www.loufang.studio/pugilist_admin";//生产环境
 //  http://www.gogogofight.com/
 NSString * const Domain = @"http://www.gogogofight.com/pugilist_admin";//生产环境
+//NSString * const Domain = @"http://192.168.85.45/pugilist_admin";//内网测试环境
 //NSString * const Domain = @"http://10.11.1.117:8080/pugilist_admin";//何后台开发环境
 //NSString * const Domain = @"http://10.11.1.49/pugilist_admin";//余彧后台开发环境
-//获取新闻
+//获取新闻192.168.85.45
 NSString * const GetNewsURL = @"/api/news/getNews.do";
 
 //评论接口地址: 
