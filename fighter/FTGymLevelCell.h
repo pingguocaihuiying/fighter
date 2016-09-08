fff3c3f3130ab5a8185c5b98472cca2623ea7ce0
mapbar <Liyz@mapbardemacbook.local>
2016-09-08 10:38:01 +0800

    增加了那三个cell文件

---
+++ fighter/FTGymLevelCell.h
@@ -0,0 +1,15 @@
+//
+//  FTGymLevelCell.h
+//  fighter
+//
+//  Created by kang on 16/9/7.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTGymLevelCell : UITableViewCell
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@end
