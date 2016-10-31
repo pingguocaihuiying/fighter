b69279f7a88e5ca7c563e3b0b5b0c2090646f69c
guoyuyanmeng <kangxiaoqiangdesky@163.com>
2016-10-31 18:03:45 +0800

    训练

---
+++ fighter/FTTraineeCell.m
@@ -0,0 +1,18 @@
+//
+//  FTTraineeCell.m
+//  fighter
+//
+//  Created by kang on 2016/10/31.
+//  Copyright © 2016年 Mapbar. All rights reserved.
+//
+
+#import "FTTraineeCell.h"
+
+@implementation FTTraineeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
