1095a34b897a7670aaa9f9b3ce923da721e97c19
李懿哲 <Liyz@bogon>
2016-12-23 16:26:10 +0800

    预约私教vc，现在会先设置返回按钮的样式

--- fighter/FTGymDetailWebViewController.m
+++ fighter/FTGymDetailWebViewController.m
@@ -105,6 +105,7 @@
     [self setTips];//控制tips是否显示
     [self initBaseData];//初始化默认数据
     [self registNoti];//注册通知
+    [self setBackButtonStyle];//设置返回按钮的样式
     [self getGymDetailInfoFromServer];//获取拳馆详情
 }
 
@@ -125,6 +126,13 @@
 - (void)registNoti{
     //注册通知，接收登录成功的消息
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(loginCallBack:) name:LoginNoti object:nil];
+}
+
+- (void)setBackButtonStyle{
+    //设置返回按钮
+    UIBarButtonItem *leftButton = [[UIBarButtonItem alloc]initWithImage:[[UIImage imageNamed:@"头部48按钮一堆-返回"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] style:UIBarButtonItemStyleDone target:self action:@selector(backBtnAction:)];
+    [leftButton setImageInsets:UIEdgeInsetsMake(0, -10, 0, 10)];
+    self.navigationItem.leftBarButtonItem = leftButton;
 }
 
 - (void)getVIPInfo{
@@ -289,10 +297,7 @@
     
     self.navigationController.navigationBar.barTintColor = [UIColor blackColor];
     
-    //设置返回按钮
-    UIBarButtonItem *leftButton = [[UIBarButtonItem alloc]initWithImage:[[UIImage imageNamed:@"头部48按钮一堆-返回"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] style:UIBarButtonItemStyleDone target:self action:@selector(backBtnAction:)];
-    [leftButton setImageInsets:UIEdgeInsetsMake(0, -10, 0, 10)];
-    self.navigationItem.leftBarButtonItem = leftButton;
+
     
     // 导航栏转发按钮
     _joinVIPButton = [[UIBarButtonItem alloc]initWithTitle:@"成为会员" style:UIBarButtonItemStylePlain target:self action:@selector(becomeVIPButtonClicked:)];
