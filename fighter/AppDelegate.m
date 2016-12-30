2601a86caea0a7f9c7753fcac94a3377e8e54210
kangxq <1515843677@qq.com>
2016-12-30 16:04:15 +0800

    update code

--- fighter/AppDelegate.m
+++ fighter/AppDelegate.m
@@ -365,9 +365,9 @@
     [IXPushSdkApi registerDeviceToken:deviceToken
                               channel:@"test" version:@"1.0" appId:1104119343];
     
-    // 格斗东西 正式版
-    [IXPushSdkApi registerDeviceToken:deviceToken
-                              channel:@"test" version:@"1.0" appId:1229394843];
+//    // 格斗东西 正式版
+//    [IXPushSdkApi registerDeviceToken:deviceToken
+//                              channel:@"test" version:@"1.0" appId:1229394843];
     
 }
 
